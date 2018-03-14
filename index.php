40ce71af8f8cf17ed3e16b640f28aee7faf386af
xiaoz <xiaoz93@outlook.com>
2018-03-14 11:53:37 +0800

    del

--- index.php
+++ index.php
@@ -119,6 +119,6 @@
 	<!--底部END-->
 	<script src="https://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
 	<script src="./layui/layui.js"></script>
-	<script src = "./static/embed.js?up=1.1"></script>
+	<script src = "./static/embed.js?up=1.2"></script>
 </body>
 </html>
